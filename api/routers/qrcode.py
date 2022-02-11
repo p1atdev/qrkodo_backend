8d0a2bec442183beb94eb67cf21197282e4b05b0
p1atdev <shu_teiei@outlook.jp>
2022-02-11 12:52:05 +0900

    feat: support svg

--- api/routers/qrcode.py
+++ api/routers/qrcode.py
@@ -1,8 +1,11 @@
+from cmath import e
 from fastapi import APIRouter
+from fastapi import HTTPException
 from fastapi.responses import StreamingResponse
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers import  (
+from qrcode.image.svg import SvgImage, SvgFillImage, SvgFragmentImage, SvgPathImage, SvgPathFillImage
+from qrcode.image.styles.moduledrawers import (
     SquareModuleDrawer,
     GappedSquareModuleDrawer,
     RoundedModuleDrawer,
@@ -16,39 +19,118 @@ import api.schemas.qrcode as qrcode_schema
 
 router = APIRouter()
 
-def generate_qrcode_from(text, size=32, border=4, fill_color="#000000", back_color="#ffffff", drawer=qrcode_schema.QRCodeDrawer.Square):
+
+def generate_qrcode_from(
+    text: str,
+    version: int,
+    error_correction: qrcode_schema.QRCodeErrorCorrection,
+    size: int,
+    border: int,
+    fill_color: str,
+    back_color: str,
+    drawer: qrcode_schema.QRCodeDrawer,
+    file_type: qrcode_schema.QRCodeFileType,
+):
+    _error_correction = None
+    if error_correction == qrcode_schema.QRCodeErrorCorrection.L:
+        _error_correction = qrcode.constants.ERROR_CORRECT_L
+    elif error_correction == qrcode_schema.QRCodeErrorCorrection.M:
+        _error_correction = qrcode.constants.ERROR_CORRECT_M
+    elif error_correction == qrcode_schema.QRCodeErrorCorrection.Q:
+        _error_correction = qrcode.constants.ERROR_CORRECT_Q
+    elif error_correction == qrcode_schema.QRCodeErrorCorrection.H:
+        _error_correction = qrcode.constants.ERROR_CORRECT_H
+    else:
+        raise HTTPException(status_code=400, detail="Invalid error_correction")
+
+    _drawer = None
+    if drawer == qrcode_schema.QRCodeDrawer.Square:
+        _drawer = SquareModuleDrawer()
+    elif drawer == qrcode_schema.QRCodeDrawer.GappedSquare:
+        _drawer = GappedSquareModuleDrawer()
+    elif drawer == qrcode_schema.QRCodeDrawer.Circle:
+        _drawer = CircleModuleDrawer()
+    elif drawer == qrcode_schema.QRCodeDrawer.Rounded:
+        _drawer = RoundedModuleDrawer()
+    elif drawer == qrcode_schema.QRCodeDrawer.VerticalBars:
+        _drawer = VerticalBarsDrawer()
+    elif drawer == qrcode_schema.QRCodeDrawer.HorizontalBars:
+        _drawer = HorizontalBarsDrawer()
+    else:
+        raise HTTPException(status_code=400, detail="Invalid drawer")
+
+    _factory = None
+    if file_type == qrcode_schema.QRCodeFileType.PNG:
+        _factory = StyledPilImage
+    elif file_type == qrcode_schema.QRCodeFileType.SVG:
+        _factory = SvgImage
+    elif file_type == qrcode_schema.QRCodeFileType.SVG_FRAGMENT:
+        _factory = SvgFragmentImage
+    elif file_type == qrcode_schema.QRCodeFileType.SVG_PATH:
+        _factory = SvgPathImage
+    else:
+        raise HTTPException(status_code=400, detail="Invalid file_type")
+
     qr = qrcode.QRCode(
-        version=None,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        version=version,
+        error_correction=_error_correction,
         box_size=size,
         border=border,
     )
     qr.add_data(text)
     qr.make(fit=True)
 
-    module_drawer = None
-    if drawer == qrcode_schema.QRCodeDrawer.Square:
-        module_drawer = SquareModuleDrawer()
-    elif drawer == qrcode_schema.QRCodeDrawer.GappedSquare:
-        module_drawer = GappedSquareModuleDrawer()
-    elif drawer == qrcode_schema.QRCodeDrawer.Circle:
-        module_drawer = CircleModuleDrawer()
-    elif drawer == qrcode_schema.QRCodeDrawer.Rounded:
-        module_drawer = RoundedModuleDrawer()
-    elif drawer == qrcode_schema.QRCodeDrawer.VerticalBars:
-        module_drawer = VerticalBarsDrawer()
-    elif drawer == qrcode_schema.QRCodeDrawer.HorizontalBars:
-        module_drawer = HorizontalBarsDrawer()
-    else:
-        module_drawer = SquareModuleDrawer()
-
-    img = qr.make_image(fill_color=fill_color, back_color=back_color, image_factory=StyledPilImage, module_drawer=module_drawer)
+    img = qr.make_image(
+        fill_color=fill_color,
+        back_color=back_color,
+        image_factory=_factory,
+        module_drawer=_drawer,
+    )
     return img
+
 
 @router.post("/api/qrcode")
 async def generate_qrcode(param: qrcode_schema.QRGenerateParameter):
-    img = generate_qrcode_from(param.text, param.size, param.border, param.fill_color, param.back_color, param.drawer)
-    stream = io.BytesIO()
-    img.save(stream, format="PNG")
-    stream.seek(0)
-    return StreamingResponse(stream, media_type="image/png")
+    # validate the param
+    if param.version > 40 or param.version < 1:
+        raise HTTPException(status_code=400, detail="version must be between 1 and 40")
+    if param.border < 0:
+        raise HTTPException(status_code=400, detail="border must be greater than 0")
+
+    img = generate_qrcode_from(
+        text=param.text,
+        version=param.version,
+        error_correction=param.error_correction,
+        size=param.size,
+        border=param.border,
+        fill_color=param.fill_color,
+        back_color=param.back_color,
+        drawer=param.drawer,
+        file_type=param.file_type,
+    )
+
+    if param.file_type == qrcode_schema.QRCodeFileType.PNG:
+        image = io.BytesIO()
+        img.save(image, format="PNG")
+        image.seek(0)
+        return StreamingResponse(
+            image,
+            media_type="image/png",
+            headers={"Content-Disposition": "inline; filename=qrcode.png"},
+        )
+    elif param.file_type in [
+        qrcode_schema.QRCodeFileType.SVG,
+        qrcode_schema.QRCodeFileType.SVG_FRAGMENT,
+        qrcode_schema.QRCodeFileType.SVG_PATH,
+    ]:
+        stream = io.BytesIO()
+        img.save(stream=stream)
+        stream.seek(0)
+        svg = stream
+        return StreamingResponse(
+            svg,
+            media_type="image/svg+xml",
+            headers={"Content-Disposition": "inline; filename=qrcode.svg", "Content-Type": "image/svg+xml"},
+        )
+    else:
+        raise HTTPException(status_code=400, detail="Invalid file_type")
--- api/schemas/qrcode.py
+++ api/schemas/qrcode.py
@@ -1,13 +1,8 @@
 import enum
 from typing import Optional
-from click import style
-import qrcode
-from qrcode.image.styledpil import StyledPilImage
-from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
-from qrcode.image.styles.colormasks import RadialGradiantColorMask
 
-from fastapi.responses import StreamingResponse
 from pydantic import BaseModel, Field
+
 
 class QRCodeDrawer(enum.Enum):
     Square: str = "Square"
@@ -17,13 +12,32 @@ class QRCodeDrawer(enum.Enum):
     VerticalBars: str = "VerticalBars"
     HorizontalBars: str = "HorizontalBars"
 
+
+class QRCodeErrorCorrection(enum.Enum):
+    L: str = "L"
+    M: str = "M"
+    Q: str = "Q"
+    H: str = "H"
+
+
+class QRCodeFileType(enum.Enum):
+    PNG: str = "png"
+    SVG: str = "svg"
+    SVG_FRAGMENT: str = "svg_fragment"
+    SVG_PATH: str = "svg_path"
+    # JPG: str = "jpg"
+
+
 class QRCodeStyle(BaseModel):
-    size: int = Field(example=32, description="QRコードのサイズ")
-    border: int = Field(example=4, description="QRコードの枠")
-    fill_color: str = Field(default="#000000", description="QRコードの色")
-    back_color: str = Field(default="#ffffff", description="QRコードの背景色")
-    drawer: QRCodeDrawer = Field(default=QRCodeDrawer.Square, description="QRコードの描画方法")
+    version: Optional[int] = Field(example=1, description="QRコードサイズ")
+    error_correction: Optional[QRCodeErrorCorrection] = Field(example=QRCodeErrorCorrection.M, description="誤り訂正レベル")
+    size: Optional[int] = Field(example=10, description="QRコードのセルのサイズ")
+    border: Optional[int] = Field(example=4, description="QRコードの枠")
+    fill_color: Optional[str] = Field(default="#000000", description="QRコードの色")
+    back_color: Optional[str] = Field(default="#ffffff", description="QRコードの背景色")
+    drawer: Optional[QRCodeDrawer] = Field(default=QRCodeDrawer.Square, description="QRコードの描画方法")
+    file_type: Optional[QRCodeFileType] = Field(default=QRCodeFileType.PNG, description="出力ファイルの種類")
+
 
 class QRGenerateParameter(QRCodeStyle):
     text: str = Field(..., example="https://example.com", description="QRコードの文字列")
-
